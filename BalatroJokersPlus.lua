98398e54cad35cfb552807803e13952a75d05b41
KaviD-115 <KaviDGaming115@gmail.com>
2024-09-19 07:47:35 -0400

    Update BalatroJokersPlus.lua
    Updated mod description

--- BalatroJokersPlus.lua
+++ BalatroJokersPlus.lua
@@ -2,7 +2,7 @@
 --- MOD_NAME: BalatroJokersPlus
 --- MOD_ID: BalatroJokersPlus
 --- MOD_AUTHOR: [KaviD]
---- MOD_DESCRIPTION: Adds 6 Thematical Balatro Jokers
+--- MOD_DESCRIPTION: Adds several Jokers to your game that aim to look and feel Vanilla.
 --- BADGE_COLOR: 191970
 --- DISPLAY_NAME: Balatro Jokers PLUS
 --- VERSION: 1.0.0
